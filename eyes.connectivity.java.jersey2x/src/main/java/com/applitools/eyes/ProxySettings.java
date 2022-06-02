2afc456b41f457010bd343a62db81b0efabe6313
KananAli96 <kanan.alizada@applitools.com>
2022-06-02 16:38:11 +0400

    rename httpOnly to isHttpOnly

--- eyes.connectivity.java.jersey2x/src/main/java/com/applitools/eyes/ProxySettings.java
+++ eyes.connectivity.java.jersey2x/src/main/java/com/applitools/eyes/ProxySettings.java
@@ -2,6 +2,10 @@ package com.applitools.eyes;
 
 public class ProxySettings extends AbstractProxySettings {
 
+    public ProxySettings(String uri, int port, String username, String password, Boolean isHttpOnly) {
+        super(uri + ":" + port, port, username, password, isHttpOnly);
+    }
+
     public ProxySettings(String uri, int port, String username, String password) {
         super(uri + ":" + port, port, username, password);
     }
