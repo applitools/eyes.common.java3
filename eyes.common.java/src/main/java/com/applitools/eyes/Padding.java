b2973674e9285cce1c3076996bb8009bf94d3b5d
IdosApplitools <ido.elmaleh@applitools.com>
2022-09-18 16:06:41 +0300

    Padding clone

--- eyes.common.java/src/main/java/com/applitools/eyes/Padding.java
+++ eyes.common.java/src/main/java/com/applitools/eyes/Padding.java
@@ -24,19 +24,56 @@ public class Padding {
         this.left = left;
     }
 
+    public Padding clone() {
+        Padding clone = new Padding();
+        clone.left = this.left;
+        clone.top = this.top;
+        clone.right = this.right;
+        clone.bottom = this.bottom;
+        return clone;
+    }
+
     public int getLeft() {
         return left;
+    }
+
+    public Padding setLeft(int left) {
+        Padding clone = this.clone();
+        clone.left = left;
+
+        return clone;
     }
 
     public int getTop() {
         return top;
     }
 
+    public Padding setTop(int top) {
+        Padding clone = this.clone();
+        clone.top = top;
+
+        return clone;
+    }
+
     public int getRight() {
         return right;
     }
 
+    public Padding setRight(int right) {
+        Padding clone = this.clone();
+        clone.right = right;
+
+        return clone;
+    }
+
     public int getBottom() {
         return bottom;
+    }
+
+    public Padding setBottom(int bottom) {
+        Padding clone = this.clone();
+        clone.bottom = bottom;
+
+        return clone;
     }
 }
