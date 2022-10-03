f7ec100c676eb08b21933cf939a16f2db6a6f7ff
IdosApplitools <ido.elmaleh@applitools.com>
2022-10-03 14:42:38 +0300

    replace removed abstract autProxy with AutProxySettings

--- eyes.common.java/src/main/java/com/applitools/eyes/AutProxySettings.java
+++ eyes.common.java/src/main/java/com/applitools/eyes/AutProxySettings.java
@@ -1,16 +1,46 @@
 package com.applitools.eyes;
 
-public class AutProxySettings extends AbstractAutProxySettings {
+public class AutProxySettings {
 
+    protected AbstractProxySettings abstractProxySettings;
+    protected String[] domains;
+    protected AutProxyMode autProxyMode;
+
+    /**
+     * Setting a separated proxy for requests made to non-eyes domains.
+     * If the AUT proxy is set, it cannot be changed again for those specific requests by any means.
+     * If AUT proxy is set to null, there will be no proxy for those specific requests.
+     */
     public AutProxySettings(AbstractProxySettings proxySettings) {
-        super(proxySettings);
+        this(proxySettings, null, null);
     }
 
+    /**
+     * Setting a separated proxy for requests made to non-eyes domains. Requests to other non-eyes domains will be sent without a proxy.
+     * If the AUT proxy is set, it cannot be changed again for those specific requests by any means.
+     * If AUT proxy is set to null, the behavior will be the same as {@link #AutProxySettings(AbstractProxySettings)}
+     */
     public AutProxySettings(AbstractProxySettings proxySettings, String[] domains) {
-        super(proxySettings, domains);
+        this(proxySettings, domains, AutProxyMode.ALLOW);
     }
 
+    /**
+     * Setting a separated proxy for requests sent to the given domains (if mode is {@link AutProxyMode#ALLOW}
+     * or for requests sent to domains other than the given domains (if mode is {@link AutProxyMode#BLOCK}.
+     * If the AUT proxy is set, it cannot be changed again for those specific requests by any means.
+     * If AUT proxy is set to null, the behavior will be the same as {@link #AutProxySettings(AbstractProxySettings)}
+     */
     public AutProxySettings(AbstractProxySettings proxySettings, String[] domains, AutProxyMode autProxyMode) {
-        super(proxySettings, domains, autProxyMode);
+        this.abstractProxySettings = proxySettings;
+        this.domains = domains;
+        this.autProxyMode = autProxyMode;
+    }
+
+    public AbstractProxySettings getProxy() { return this.abstractProxySettings; }
+
+    public String[] getDomains() { return this.domains; }
+
+    public AutProxyMode getAutProxyMode() {
+        return this.autProxyMode;
     }
 }
